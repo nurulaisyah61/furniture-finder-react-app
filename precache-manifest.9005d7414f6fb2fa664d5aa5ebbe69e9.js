65ba29d17a00ce2749bb6e1e52e16adec300df43
Nurul Aisyah <aisynurul99@gmail.com>
2020-02-16 21:50:38 +0700

    Updates

---
+++ precache-manifest.9005d7414f6fb2fa664d5aa5ebbe69e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c98edb4499da7c090ca4dfa4456b8e9",
+    "url": "/furniture-finder-react-app/index.html"
+  },
+  {
+    "revision": "7438f49e58bb61f0bf05",
+    "url": "/furniture-finder-react-app/static/css/main.b7124770.chunk.css"
+  },
+  {
+    "revision": "596b99c4cf3615407847",
+    "url": "/furniture-finder-react-app/static/js/2.c96c0459.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/furniture-finder-react-app/static/js/2.c96c0459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7438f49e58bb61f0bf05",
+    "url": "/furniture-finder-react-app/static/js/main.afa3d697.chunk.js"
+  },
+  {
+    "revision": "f0367dfd9d8486d4bbce",
+    "url": "/furniture-finder-react-app/static/js/runtime-main.dd633a47.js"
+  }
+]);
